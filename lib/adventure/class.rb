a5530ea78dd4cdfc545b9d7b62b069a968a4ee55
Igor Padoim <igorpadoim@gmail.com>
2019-08-31 14:04:15 -0300

    Change to-do notation

--- lib/adventure/class.rb
+++ lib/adventure/class.rb
@@ -5,7 +5,7 @@ module Adventure
   class RPGClass
     attr_accessor :hd, :proficiency
 
-    # TODO: Define RPGClass class
-    # BODY: Begin the class definition class.
+    # @todo: Define RPGClass class
+    # @body: Begin the class definition class.
   end
 end
