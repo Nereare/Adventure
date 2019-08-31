c236eafda7673079e64067e449d805d0fcb9fb98
Igor Padoim <igorpadoim@gmail.com>
2019-08-31 15:22:23 -0300

    Change requires' case

--- lib/adventure.rb
+++ lib/adventure.rb
@@ -1,12 +1,12 @@
 # frozen_string_literal: true
 
-require 'Adventure/version'
+require 'adventure/version'
 
-require 'Adventure/Armor'
-require 'Adventure/Class'
-require 'Adventure/Dice'
-require 'Adventure/NPC'
-require 'Adventure/Weapon'
+require 'adventure/armor'
+require 'adventure/class'
+require 'adventure/dice'
+require 'adventure/npc'
+require 'adventure/weapon'
 
 module Adventure
   class Error < StandardError; end
