44cd78474e550a5f51a8007bf953b7341b0964a7
Igor Padoim <igorpadoim@gmail.com>
2019-08-31 12:23:57 -0300

    Refactor to meet RuboCop errors

--- lib/Adventure/NPC.rb
+++ lib/Adventure/NPC.rb
@@ -1,108 +1,125 @@
 # frozen_string_literal: true
 
-class NPC
-  attr_accessor :name, :race, :clas, :str, :dex, :con, :int, :wis, :cha, :speed, :align, :hp, :languages, :resistances, :immunities, :senses, :shield, :armor, :weapons
-  attr_reader :inventory, :pp, :gp, :sp, :cp
+module Adventure
+  # Defines an NPC entity
+  class NPC
+    attr_accessor :name, :race, :clas, :str, :dex, :con, :int, :wis, :cha
+    attr_accessor :speed, :align, :hp, :languages, :resistances, :immunities
+    attr_accessor :senses, :shield, :armor, :weapons, :money
 
-  def initialize(name, race, clas, level, align, stats, armor, senses = '', extra_languages = [], weapons = [], inventory = [], cp = 0, sp = 0, gp = 0, pp = 0, resistances = '', immunities = '')
-    raise ArgumentError, 'Race argument is not a Race class' unless race.is_a? Race
-    raise ArgumentError, 'Class argument is not a Class class' unless clas.is_a? RPGClass
-    raise ArgumentError, 'Extra languages argument is not an Array' unless extra_languages.is_a? Array
-    raise ArgumentError, 'Weapons argument is not an Array' unless weapons.is_a? Array
-    raise ArgumentError, 'Inventory argument is not an Array' unless inventory.is_a? Array
-    raise ArgumentError, 'PP argument is not an Array' unless pp.is_a? Numeric
-    raise ArgumentError, 'GP argument is not an Array' unless gp.is_a? Numeric
-    raise ArgumentError, 'SP argument is not an Array' unless sp.is_a? Numeric
-    raise ArgumentError, 'CP argument is not an Array' unless cp.is_a? Numeric
+    attr_reader :inventory
 
-    @name   = name
-    @race   = race
-    @clas   = clas
-    @level  = level
-    @align  = align
-    @senses = senses
+    def initialize(name,
+                   race,
+                   clas,
+                   level,
+                   align,
+                   stats,
+                   armor,
+                   senses = '',
+                   extra_languages = [],
+                   weapons = [],
+                   inventory = [],
+                   money = { pp: 0, gp: 0, sp: 0, cp: 0 },
+                   resistances = '',
+                   immunities = '')
+      check_params(race, clas, extra_languages, weapons, inventory, money)
 
-    @str, @dex, @con, @int, @wis, @cha = stats
+      @name   = name
+      @race   = race
+      @clas   = clas
+      @level  = level.to_i
+      @align  = align
+      @senses = senses
 
-    @resistances = resistances
-    @immunities  = immunities
+      @str, @dex, @con, @int, @wis, @cha = stats
 
-    @hp = Dice.roll(@level.to_s + @clas.hd) + getModif(@con).to_i * @level.to_i
+      @resistances = resistances
+      @immunities  = immunities
 
-    @pp = pp
-    @gp = gp
-    @sp = sp
-    @cp = cp
+      new_hp
 
-    @speed = @race.speed
+      @money = money
 
-    @armor = armor
-    @weapons = weapons
-    @inventory = inventory
-  end
+      @speed = @race.speed
 
-  def getModif(score)
-    ((score.to_f - 10.5) / 2.0).round(0).to_i
-  end
+      @armor = armor
+      @weapons = weapons
+      @inventory = inventory
+    end
 
-  def get(item)
-    @inventory.push(item)
-  end
+    def check_params(race,
+                     clas,
+                     extra_languages,
+                     weapons,
+                     inventory,
+                     money)
+      race_ok? race, 'Race argument is not a Race class'
+      class_ok? clas, 'Class argument is not a Class class'
+      languages_ok? extra_languages, 'Extra languages argument is not an Array'
+      weapons_ok? weapons, 'Weapons argument is not an Array'
+      inventory_ok? inventory, 'Inventory argument is not an Array'
+      money_ok? money, 'Money argument must define all four currencies'
 
-  def drop(item)
-    @inventory.delete(item)
-  end
+      true
+    end
 
-  def setPP(pp)
-    @pp = pp.to_i
-  end
+    def race_ok?(race, msg)
+      raise ArgumentError, msg.to_s unless race.is_a? Adventure::Race
+    end
 
-  def setGP(gp)
-    @gp = gp.to_i
-  end
+    def class_ok?(clas, msg)
+      raise ArgumentError, msg.to_s unless clas.is_a? Adventure::RPGClass
+    end
 
-  def setSP(sp)
-    @sp = sp.to_i
-  end
+    def languages_ok?(extra_languages, msg)
+      raise ArgumentError, msg.to_s unless extra_languages.is_a? Array
+    end
 
-  def setCP(cp)
-    @cp = cp.to_i
-  end
+    def weapons_ok?(weapons, msg)
+      raise ArgumentError, msg.to_s unless weapons.is_a? Array
+    end
 
-  def addPP(pp)
-    @pp += pp.to_i
-  end
+    def inventory_ok?(inventory, msg)
+      raise ArgumentError, msg.to_s unless inventory.is_a? Array
+    end
 
-  def addGP(gp)
-    @gp += gp.to_i
-  end
+    def money_ok?(money, msg)
+      plat_gold = money.respond_to?[:pp] && money.respond_to?[:gp]
+      silv_copp = money.respond_to?[:sp] && money.respond_to?[:cp]
+      raise ArgumentError, msg.to_s unless plat_gold && silv_copp
+    end
 
-  def addSP(sp)
-    @sp += sp.to_i
-  end
+    def new_hp
+      @hp = Adventure::Dice.roll(@level.to_s + @clas.hd)
+      @hp += get_modif(@con).to_i * @level.to_i
+    end
 
-  def addCP(cp)
-    @cp += cp.to_i
-  end
+    def get_modif(score)
+      ((score.to_f - 10.5) / 2.0).round(0).to_i
+    end
 
-  def remPP(pp)
-    @pp -= pp.to_i
-  end
+    def get(item)
+      @inventory.push(item)
+    end
 
-  def remGP(gp)
-    @gp -= gp.to_i
-  end
+    def drop(item)
+      @inventory.delete(item)
+    end
 
-  def remSP(sp)
-    @sp -= sp.to_i
-  end
+    def total_money
+      pp, gp, sp, cp = @money.values
+      (pp * 10 + gp + sp.to_f / 10.0 + cp.to_f / 100.0).round(2)
+    end
 
-  def remCP(cp)
-    @cp -= cp.to_i
-  end
+    def finances
+      pp, gp, sp, cp = @money.values
+      s = "#{total_money}gp (#{pp ? pp.to_s + 'pp, ' : ''}"\
+          "#{gp ? gp.to_s + 'gp, ' : ''}"\
+          "#{sp ? sp.to_s + 'sp, ' : ''}"\
+          "#{cp ? cp.to_s + 'cp, ' : ''}"
 
-  def finances
-    total = (@pp * 10 + @gp + @sp.to_f / 10.0 + @cp.to_f / 100.0).round(2)
-    (total.to_s + 'gp (' + (@pp > 0 ? @pp.to_s + 'pp, ' : '') + (@gp > 0 ? @gp.to_s + 'gp, ' : '') + (@sp > 0 ? @sp.to_s + 'sp, ' : '') + (@cp > 0 ? @cp.to_s + 'cp, ' : '')).chomp(', ') + ')'
+      s.chomp!(', ').concat(')')
+    end
   end
 end
