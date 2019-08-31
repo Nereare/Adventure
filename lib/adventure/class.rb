f0349d8eabd6f27c400ea151d9ae25fc030940e0
Igor Padoim <igorpadoim@gmail.com>
2019-08-31 14:02:14 -0300

    Add to-do comment

--- lib/adventure/class.rb
+++ lib/adventure/class.rb
@@ -4,5 +4,8 @@ module Adventure
   # Defines a PC or NPC class.
   class RPGClass
     attr_accessor :hd, :proficiency
+
+    # TODO: Define RPGClass class
+    # BODY: Begin the class definition class.
   end
 end
