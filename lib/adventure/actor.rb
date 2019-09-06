11f07b1e433f07c78564167e9194f8c404a449eb
Igor Padoim <igorpadoim@gmail.com>
2019-09-06 13:35:59 -0300

    Add Actor class YARDoc

--- lib/adventure/actor.rb
+++ lib/adventure/actor.rb
@@ -8,6 +8,19 @@ module Adventure
     attr_reader :name, :gender, :race, :align, :hair, :eyes, :skin, :personality
     attr_reader :equipment, :reference
 
+    # Creates a new Actor (NPC) instance.
+    #
+    # @param name [String] The name of the NPC.
+    # @param gender [String] One of 'female', 'male' or others.
+    # @param race [String] The race of the NPC.
+    # @param align [String] The alignment of the character (both moral and
+    #   ethical)
+    # @param desc [Array] An array of strings, in the order: hair color, eye
+    #   color, skin color and personality.
+    # @param equipment [Array] An array of strings, each element being one worn
+    #   item.
+    # @param ref [String or false] Either a reference to the SRD, WotC books or
+    #   other source with further information.
     def initialize(name, gender, race, align, desc, equipment, ref = false)
       @name = name
       @gender = gender
@@ -23,6 +36,10 @@ module Adventure
       process_equipment
     end
 
+    # Constructs the description of the NPC in human-readable format.
+    #
+    # @param upcase [Boolean] If the description must begin capitalized.
+    # @return [String] The description of the NPC.
     def description(upcase = false)
       "#{pronoum(upcase)} is #{@align.indefinitize} #{@gender} #{@race}, whose"\
       " hair is #{@hair}, with #{@eyes} eyes and #{@skin} skin. "\
@@ -30,6 +47,9 @@ module Adventure
       "#{@personality.chomp '.'}."
     end
 
+    # Returns the inventory of the NPC in human-readable format.
+    #
+    # @return [String] The inventory of the NPC.
     def inventory
       case @equip.length
       when 1
