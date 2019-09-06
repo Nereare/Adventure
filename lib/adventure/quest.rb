44b30c846a3398edcc5afa35e96277f19e0953b1
Igor Padoim <igorpadoim@gmail.com>
2019-09-06 14:40:30 -0300

    Add Quest class YARDoc

--- lib/adventure/quest.rb
+++ lib/adventure/quest.rb
@@ -7,6 +7,13 @@ module Adventure
   class Quest
     attr_reader :name, :description, :goal, :reward, :dungeon
 
+    # Creates a new instance of Quest.
+    #
+    # @param name [String] The name of the Quest.
+    # @param description [String] The description and history of the Quest.
+    # @param goal [String] The goal of the Quest.
+    # @param reward [String] The reward or rewards of completing the Quest.
+    # @param dungeon [Dungeon] The dungeon where the Quest takes place.
     def initialize(name, description, goal, reward, dungeon)
       @name = name.capitalize
       @description = description
@@ -16,6 +23,9 @@ module Adventure
       check_dungeon(dungeon)
     end
 
+    # Returns the first {Room} of the {Dungeon} where the Quest takes place.
+    #
+    # @return [Room] The entry point of the {Dungeon} for this Quest.
     def begin
       @dungeon.first
     end
