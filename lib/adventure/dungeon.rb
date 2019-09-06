3bbc15ffaaa4d52c233fcca83345ec8c6cda714a
Igor Padoim <igorpadoim@gmail.com>
2019-09-06 14:20:23 -0300

    Add Dungeon class YARDoc

--- lib/adventure/dungeon.rb
+++ lib/adventure/dungeon.rb
@@ -7,10 +7,17 @@ module Adventure
   class Dungeon
     attr_reader :name, :first
 
+    # Creates a new instance of a Dungeon, which is a category for {Room}s and
+    # sets an entry point.
+    #
+    # @param name [String] The name of the dungeon.
     def initialize(name)
       @name = name.capitalize
     end
 
+    # Sets the entry point {Room} to the dungeon.
+    #
+    # @param room [Room] The entry {Room}.
     def first=(room)
       raise ArgumentError, 'Must be a Room' unless room.is_a? Room
 
