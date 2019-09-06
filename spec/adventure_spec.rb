ce17aa1896eb57d3472fec63b58d8afb38517128
Igor Padoim <igorpadoim@gmail.com>
2019-09-06 18:27:52 -0300

    Add Actor class checks, works on #7

--- spec/adventure_spec.rb
+++ spec/adventure_spec.rb
@@ -7,3 +7,108 @@ RSpec.describe Adventure do
     expect(Adventure.version).not_to be nil
   end
 end
+
+RSpec.describe Adventure::Actor do
+  context 'when creation follows documentation' do
+    let(:sam) do
+      Adventure::Actor.new(
+        'Sam',
+        'Male',
+        'Halfling',
+        'Lawful good',
+        [
+          'Brown',
+          'Curly brown',
+          'Rosey-white',
+          'stubby, hearty, and steadfast of heart'
+        ],
+        ['cutlery kit', 'spices', 'bread']
+      )
+    end
+
+    let(:lady) do
+      Adventure::Actor.new(
+        'Lady of the Forest',
+        'Female',
+        'Elf',
+        'Lawful neutral',
+        [
+          'Blue',
+          'Bright blonde',
+          'Pale',
+          'Silent and powerful'
+        ],
+        ['light of a beloved star']
+      )
+    end
+
+    let(:smeagol) do
+      Adventure::Actor.new(
+        'Smeagol',
+        'Neural',
+        'Halfling',
+        'Chaotic neutral',
+        [
+          'Blue',
+          'No',
+          'Sickly gray',
+          'Suspicious of others'
+        ],
+        %w[fish precious]
+      )
+    end
+
+    it 'creates NPCs (Actors)' do
+      expect(sam).not_to be nil
+      expect(lady).not_to be nil
+      expect(smeagol).not_to be nil
+      expect(sam).to be_a Adventure::Actor
+      expect(lady).to be_a Adventure::Actor
+      expect(smeagol).to be_a Adventure::Actor
+    end
+
+    it 'describes NPCs (Actors)' do
+      expect(sam.description).to be_a String
+      expect(lady.description).to be_a String
+      expect(smeagol.description).to be_a String
+    end
+  end
+
+  context 'when creation ignores documentation' do
+    it 'checks the description argument' do
+      expect do
+        Adventure::Actor.new(
+          'Name',
+          'Gender',
+          'Race',
+          'Align',
+          'Wrong type for "desc" paramenter',
+          %w[Item]
+        )
+      end .to raise_error ArgumentError
+      expect do
+        Adventure::Actor.new(
+          'Name',
+          'Gender',
+          'Race',
+          'Align',
+          %w[Eyes Hair Skin],
+          %w[Item]
+        )
+      end .to raise_error ArgumentError
+    end
+
+    it 'checks the inventory argument' do
+      expect do
+        Adventure::Actor.new(
+          'Name',
+          'Gender',
+          'Race',
+          'Align',
+          %w[Eyes Hair Skin Personality],
+          'Wrong item'
+        )
+      end .to raise_error ArgumentError
+    end
+  end
+end
