a8c80fc3da313d21633c00360068b6ca80ac2e2f
Igor Padoim <igorpadoim@gmail.com>
2019-09-06 19:04:19 -0300

    Add Building class checks, works on #7

--- spec/adventure_spec.rb
+++ spec/adventure_spec.rb
@@ -62,9 +62,9 @@ RSpec.describe Adventure::Actor do
       expect(sam).not_to be nil
       expect(lady).not_to be nil
       expect(smeagol).not_to be nil
-      expect(sam).to be_a Adventure::Actor
-      expect(lady).to be_a Adventure::Actor
-      expect(smeagol).to be_a Adventure::Actor
+      expect(sam).to be_an Adventure::Actor
+      expect(lady).to be_an Adventure::Actor
+      expect(smeagol).to be_an Adventure::Actor
     end
 
     it 'describes NPCs (Actors)' do
@@ -107,6 +107,89 @@ RSpec.describe Adventure::Actor do
           'Align',
           %w[Eyes Hair Skin Personality],
           'Wrong item'
+        )
+      end .to raise_error ArgumentError
+    end
+  end
+end
+
+RSpec.describe Adventure::Building do
+  context 'when creation follows documentation' do
+    let(:example) do
+      Adventure::Building.new(
+        'The Prancing Pony',
+        'East Road',
+        'A large wooden building with rooms for humans and halflings alike',
+        'Tavern',
+        Adventure::Actor.new(
+          'Barliman',
+          'Male',
+          'Human',
+          'Lawful good',
+          %w[Brown Brown White Absent-minded],
+          %w[Apron Cloth]
+        ),
+        nil,
+        [
+          ['Ale Mug', '1cp'],
+          ['Potato Soup', '2cp']
+        ]
+      )
+    end
+
+    it 'creates buildings' do
+      expect(example).not_to be nil
+      expect(example).to be_an Adventure::Building
+      expect(example.owner).to be_an Adventure::Actor
+    end
+
+    it 'outputs Jekyll-ready IDs' do
+      expect(example.id).to match(/[0-9a-z\-]+/)
+    end
+
+    it 'outputs descriptions of the building' do
+      expect(example.description).to be_a String
+    end
+  end
+
+  context 'when creation ignores documentation' do
+    it 'checks the owner paramenter' do
+      expect do
+        Adventure::Building.new(
+          'The Prancing Pony',
+          'East Road',
+          'A large wooden building with rooms for humans and halflings alike',
+          'Tavern',
+          'Barliman',
+          nil,
+          [
+            ['Ale Mug', '1cp'],
+            ['Potato Soup', '2cp']
+          ]
+        )
+      end .to raise_error ArgumentError
+    end
+
+    it 'checks the quests paramenter' do
+      expect do
+        Adventure::Building.new(
+          'The Prancing Pony',
+          'East Road',
+          'A large wooden building with rooms for humans and halflings alike',
+          'Tavern',
+          Adventure::Actor.new(
+            'Barliman',
+            'Male',
+            'Human',
+            'Lawful good',
+            %w[Brown Brown White Absent-minded],
+            %w[Apron Cloth]
+          ),
+          ['Error'],
+          [
+            ['Ale Mug', '1cp'],
+            ['Potato Soup', '2cp']
+          ]
         )
       end .to raise_error ArgumentError
     end
